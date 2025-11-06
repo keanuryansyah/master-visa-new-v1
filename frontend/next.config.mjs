1b8732d0006997fc0a360786ff482d7364aac469
Zed <kembedt@gmail.com>
2025-11-06 21:04:43 +0700

    change env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: "standalone",
+  output: 'standalone',
   reactCompiler: true,
   env: {
-    NEXT_PUBLIC_API_URL: "http://localhost:4000",
-    // NEXT_PUBLIC_API_URL: "https://be.mastervisaku.com",
+    // NEXT_PUBLIC_API_URL: 'http://localhost:4000',
+    NEXT_PUBLIC_API_URL: 'https://be.mastervisaku.com',
   },
 };
 
